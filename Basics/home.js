ee815490afabeede151f38204c57f7ea23dc412a
Ritveak Dugar <ritveak@myidos.com>
2020-05-29 12:00:06 +0530

    fool proof now

--- Basics/home.js
+++ Basics/home.js
@@ -1,6 +1,10 @@
 
 //------------------stubborn prompt-----------------------
 function one (){
+    document.getElementById("one").disabled=true;
+    document.getElementById("two").disabled=false;
+    document.getElementById("content").innerHTML="";
+    
     document.getElementById("title").innerHTML="Hey there "
     var name = prompt("What is your name?")
     while(name==null || name =="")
@@ -15,11 +19,15 @@ function one (){
     // {
     //     return prompt("No!, you are supposed to enter your name u dumbo!")
     // }
+    
 }
 
 // -----------------age in days------------------buttons created using javascript
+//haven't considered leap years----------__doing this for js not competitive coding.
 function two(){
     document.getElementById("two").disabled=true;
+    document.getElementById("one").disabled=false;
+
 
     document.getElementById("title").innerHTML="Let's get your age in days"
     
@@ -28,6 +36,7 @@ function two(){
     text.classList="text-center martop";
     document.getElementById("content").appendChild(text);
     var enterAge = document.createElement("BUTTON");
+    enterAge.id="ageinp";
     enterAge.innerHTML = "Enter age";
     enterAge.classList="btn btn-primary text-center martop"
     enterAge.onclick =askage;
@@ -38,6 +47,6 @@ function two(){
 function askage(){
     var y = prompt("Enter your age");
     document.getElementById("inp").innerHTML="Your age in years is "+y+"<br><br> Your age in days is "+y*365;
-
+    document.getElementById("ageinp").innerHTML="Update Age";
 
 }
